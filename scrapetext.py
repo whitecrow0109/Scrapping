6e5048caed9f95350891030a06a2f004251e37bd
whitecrow0109 <whitecrow0109@gmail.com>
2023-04-10 03:53:43 +0900

    Handling Exceptions

--- scrapetext.py
+++ scrapetext.py
@@ -1,4 +1,23 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+from bs4 import BeautifulSoup
 
-html = urlopen('http://pythonscraping.com/pages/page1.html')
-print(html.read())
+def getTitle(url):
+    try:
+        page = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        soup = BeautifulSoup(page.read(), 'html.parser')
+        title = soup.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+
+title = getTitle('http://pythonscraping.com/pages/page1.html')
+if title == None:
+    print("Title couldn't be found")
+else:
+    print(title)
